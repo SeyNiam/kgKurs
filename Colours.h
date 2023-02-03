c62578bb5bdec4b8ecff023dabaf8b85268e62e1
Юлия Викторовна Ахромова <227890@niuitmo.ru>
2023-02-03 12:56:01 +0300

    Добавлены новые цвета.

--- Colours.h
+++ Colours.h
@@ -5,8 +5,13 @@
 #define WHITE 15
 #define BLACK 0
 
-#define GREEN 2
+#define DARK_GREEN 2
+#define GREEN 10
 #define CYAN 11
-#define RED	4
+#define BLUE 9
+
 #define YELLOW	14
+#define DARK_RED	4
+#define RED	12
+#define DARK_MAGENTA 5
 #define MAGENTA	13
